b23ca6e6c609c0168f4988b4ce880350f260fb4f
Aurea Martinez <aurea@codeforprogress.org>
2014-05-20 14:37:34 -0400

    Added Heroku files

--- app/views.py
+++ app/views.py
@@ -3,8 +3,17 @@
 from app import app
 from flask import render_template
 
-@app.route('/')
-
-def index():
+@app.route('/DC')
+def DC():
 	return render_template('DC.html')
+	
+
+@app.route('/DC')
+def Lawyer():
+	return render_template('Lawyer.html')
+	
+
+@app.route('/DC')
+def PageTwo():
+	return render_template('PageTwo.html')
 	
